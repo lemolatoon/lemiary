8375c4f5387f0aabcbacd38021b18c646fc645fa
lemolatoon <63438515+lemolatoon@users.noreply.github.com>
2022-09-10 12:58:04 +0900

    Remove axios

--- frontend/src/submitPage.tsx
+++ frontend/src/submitPage.tsx
@@ -1,8 +1,3 @@
-import axios from 'axios';
-
-// axios.defaults.baseURL = 'http://localhost:3001';
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 export const SubmitPage = () => {
     return (
         <>
